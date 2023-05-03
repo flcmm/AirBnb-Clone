31a8a17687d2ba7a0695fb4065829bceab81ac43
froilan <61849282+flcmm@users.noreply.github.com>
2023-05-03 14:22:53 +0000

    added button element

---
+++ app/components/button.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+interface buttonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  // custom properties goes inside here
+  disabled?: boolean
+  text: string
+}
+
+const Button: FC<buttonProps> = ({disabled, text, ...props}) => {
+  return <button disabled>{text}</button>
+}
+
+export default Button
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navigation from './components/navigation'
+import Button from './components/button'
+import Filters from './components/filterpane/filters'
 
 export default function Home() {
   return (
     <>
       <div className=''>
         <Navigation />
+        <Filters />
       </div>
     </>
   )
