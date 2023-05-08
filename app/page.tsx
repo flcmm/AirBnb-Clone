09c31e94b14ce3356b56c70f17d3c22977700ebd
froilan aquino <froilanaquino1@gmail.com>
2023-05-08 14:49:59 +0800

    fix import names

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Navigation from './components/navigation'
 import Button from './components/button'
 import Filters from './components/filterpane/filters'
-import RegisterModal from './components/modal/RegisterModal'
+import RegisterModal from './components/modal/signupModal'
+
 export default function Home() {
   return (
     <>
