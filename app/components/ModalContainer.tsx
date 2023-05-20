9904d5d496abc02fd7dfc3827f52af7f39d2c6ae
froilan aquino <froilanaquino1@gmail.com>
2023-05-20 10:29:18 +0800

    fix typo

--- app/components/ModalContainer.tsx
+++ app/components/ModalContainer.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import SignInModal from './modal/SignInModal'
-import SignupModal from './modal/SignUpModal'
+import SignUpModal from './modal/SignUpModal'
 
 interface modalsProps {
   hidden: boolean,
@@ -11,7 +11,7 @@ const Modals: FC<modalsProps> = ({hidden, mode}) => {
   return (
     <>
       <div className={`${hidden ? 'none' : 'block backdrop-blur'} bg-black/50 w-full flex justify-center items-center h-screen z-100 absolute top-0`}>
-        {mode === 'login' ? <SignInModal /> : <SignupModal />}
+        {mode === 'login' ? <SignInModal /> : <SignUpModal />}
       </div>
     </>
   )
